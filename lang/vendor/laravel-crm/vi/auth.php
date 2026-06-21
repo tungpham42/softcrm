1adfa5e32298bfc7750e26183609850a2b71a508
Tung Pham <tung.42@gmail.com>
2026-06-21 09:51:01 +0700

    update lang

--- lang/vendor/laravel-crm/vi/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'failed' => 'Thông tin đăng nhập này không khớp với dữ liệu của chúng tôi.',
-    'throttle' => 'Quá nhiều lần thử đăng nhập. Vui lòng thử lại sau :seconds giây.',
-
-];
--- lang/vendor/laravel-crm/vi/pagination.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'previous' => '&laquo; Trước',
-    'next' => 'Tiếp theo &raquo;',
-
-];
