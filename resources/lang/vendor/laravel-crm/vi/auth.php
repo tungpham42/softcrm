4686f68f897ce0e07aa760a5ccf240eb718949f0
Tung Pham <tung.42@gmail.com>
2026-06-21 09:23:17 +0700

    translate VN

---
+++ resources/lang/vendor/laravel-crm/vi/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'failed' => 'Thông tin đăng nhập này không khớp với dữ liệu của chúng tôi.',
+    'throttle' => 'Quá nhiều lần thử đăng nhập. Vui lòng thử lại sau :seconds giây.',
+
+];
